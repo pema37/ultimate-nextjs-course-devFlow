07b48183b6f19d88e7674f63eb24e7eb25209ee9
pema37 <shawbernard281@gmail.com>
2024-11-12 07:09:28 -0800

    setup LeftSidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,21 @@
 import React, { ReactNode } from 'react';
 import Navbar from '@/components/ui/navigation/navbar';
+import LeftSideBar from '@/components/ui/navigation/LeftSideBar';
 
 const RootLayout = ({ children}: { children: ReactNode }) => {
   return (
-    <main>
+    <main className='background-light850_dark100 relative'>
       <Navbar/>
-      {children}
+
+      <div className='flex'>
+        <LeftSideBar />
+
+        <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14'>
+          {children}
+        </section>
+
+      </div>
+
     </main>
 
   )
